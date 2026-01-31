cba53174f9b0202fe3af406b2cf2e5ae6c329fc7
Cai Scheidler <86428966+Slipperee-CODE@users.noreply.github.com>
2026-01-30 19:21:18 -0800

    Update donut_drive.c

--- src/donut_drive.c
+++ src/donut_drive.c
@@ -1,13 +1,15 @@
 #include "donut_drive.h"
 
 void drive_handle_idle(){
-    
+    led_time_blink(TIME_BETWEEN_FAST_BLINKS);
 }
 
-void drive_handle_spin(){
-    
+void drive_handle_spin(double throttle){
+    motor_send_throttle(MOTOR1, throttle);
+    motor_send_throttle(MOTOR2, 1-throttle);
 }
 
-void drive_handle_tank(){
-    
+void drive_handle_tank(double left_y, double right_y){
+    motor_send_throttle(MOTOR1, left_y);
+    motor_send_throttle(MOTOR2, right_y);
 }
