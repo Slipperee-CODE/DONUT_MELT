4fd8315f19aa1eaf32ee194626313d626274860c
cai <cai_scheidler@berkeley.edu>
2026-03-15 00:09:46 -0700

    adding dshot unit test

---
+++ lib/c_pico_bidir_dshot/tests/default_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "c_pico_dshot.h"
+
+
+int main() {
+    stdio_init_all();
+
+    while (1){
+        printf("HELLO WORLD!");
+    }
+
+    return 0;
+}
