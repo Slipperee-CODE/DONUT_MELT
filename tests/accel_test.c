a1d6195317c64455c55ce488766ceecc329ff55a
cai <cai_scheidler@berkeley.edu>
2026-03-20 19:02:41 -0700

    added zero g offset to accel test

--- tests/accel_test.c
+++ tests/accel_test.c
@@ -12,9 +12,9 @@ int main() {
 
     while (1) {
         double* accel_data = accelerometer_get_all_axis();
-        printf("X axis: %lf \n", accel_data[0]);
-        printf("Y axis: %lf \n", accel_data[1]);
-        printf("Z axis: %lf \n", accel_data[2]);
+        printf("X axis: %lf \n", accel_data[0]-ACCEL_ZERO_G_OFFSET);
+        printf("Y axis: %lf \n", accel_data[1]-ACCEL_ZERO_G_OFFSET);
+        printf("Z axis: %lf \n", accel_data[2]-ACCEL_ZERO_G_OFFSET);
     }
     
     return 0;
