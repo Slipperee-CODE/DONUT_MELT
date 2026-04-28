1ac2991d98645e59df13a3442fb94267c2cdbe12
cai <cai_scheidler@berkeley.edu>
2026-04-28 12:19:29 -0700

    adjusted P value

--- src/donut_config.h
+++ src/donut_config.h
@@ -44,7 +44,7 @@ uint8_t donut_get_curr_drive_mode();
 //----------DONUT DRIVE SETTINGS---------
 
 #define SHOULD_ONLY_SMOOTH_UP 1 // 1 is yes, 0 is no
-#define THROTTLE_PDC_P 1
+#define THROTTLE_PDC_P 0.75
 #define THROTTLE_PDC_D 0.1
 
 #define CAN_ADJUST_ACCEL_MOUNT_RADIUS
