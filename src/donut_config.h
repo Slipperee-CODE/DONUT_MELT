2902c5c18dce2997ef951280d6e9cb1dd5ff8fa2
cai <cai_scheidler@berkeley.edu>
2026-04-29 21:00:07 -0700

    disabled testing flags

--- src/donut_config.h
+++ src/donut_config.h
@@ -18,9 +18,9 @@ uint8_t donut_get_curr_drive_mode();
 
 #define FAKE_RPM 60 // the fake rpm to be used in get_fake_rpm (in some capacity)
 
-#define LIE_ABOUT_RPM // fakes RPM data
+// #define LIE_ABOUT_RPM // fakes RPM data
 // #define LIE_ABOUT_INPUT // fakes receiver data
-#define NO_MOTOR_SPINNING // prevents motors from spinning up if defined
+// #define NO_MOTOR_SPINNING // prevents motors from spinning up if defined
 #define BYPASS_MIN_TRANSLATION_RPM // lets us attempt to translate under MIN_TRANSLATION_RPM if defined
 
 //----------END DEBUG----------
