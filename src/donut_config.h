c12d7e6effd046d14d985761b24a08de47624e66
cai <cai_scheidler@berkeley.edu>
2026-05-21 11:40:48 -0700

    separated 1lb and 3lb settings into 2 diff files

--- src/donut_config.h
+++ src/donut_config.h
@@ -11,6 +11,13 @@ uint8_t donut_is_killswitch_active();
 
 uint8_t donut_get_curr_drive_mode();
 
+//----------BOT TYPE----------
+
+#include "donut_1lb_config.h"
+// #include "donut_3lb_config.h"
+
+//----------END BOT TYPE----------
+
 
 //----------DEBUG SETTINGS----------
 
@@ -28,70 +35,11 @@ uint8_t donut_get_curr_drive_mode();
 
 //---------------LED SETTINGS---------------
 
-// Onboard Pico2 led is on pin 25
-// For 1lb:
-//  Our usual top led is on pin 6 
-//  Our usual bottom led is on pin 7
-// For 3lb:
-//  Our usual top AND bottom led pin is 20
-#define HEADING_LIGHT_STRIP_PIN	25
-
-// 1lb
-// #define HEADING_LIGHT_STRIP_PIN2 7
-
-// 3lb
-#define HEADING_LIGHT_STRIP_PIN2 20
-
 #define SLOW_BLINK 500
 #define FAST_BLINK 50
 #define REPEAT_BLINK 250
 
 //---------------END LED SETTINGS---------------
-
-
-//----------DONUT DRIVE SETTINGS---------
-
-#define USE_3LB_ACCEL_SETUP
-#define RPM_MULTIPLIER_MAX 2
-
-#define ACCEL_1_X_OFFSET -7
-#define ACCEL_1_Y_OFFSET -5
-
-#define ACCEL_2_X_OFFSET -7
-#define ACCEL_2_Y_OFFSET -5
-
-
-#define THROTTLE_PC_P 1 // was 0.5 for working translation test with 1lb on 4/29/26 - Cai
-
-#define CAN_ADJUST_ACCEL_MOUNT_RADIUS
-#define ACCEL_OFFSET_SENSITIVITY 0.0001
-
-// Doesn't do anything on the 3lb because of the dual accel math - Cai
-#define ACCEL_MOUNT_RADIUS_CM 2.4 + 0.3559 // 0.3559 is manually measured offset - Cai
-
-// for 1lb
-#define ACCEL_ZERO_G_OFFSET 2
-#define LEFT_RIGHT_HEADING_CONTROL_DIVISOR 0.1
-
-#define MOTOR_ON_PERCENT_DURATION 0.5 // This might technically be a half of a half - Cai
-#define MIN_TRANSLATION_RPM 400
-
-#define LED_OFFSET_PERCENT 0.25
-#define MIN_LED_PERCENT_DURATION 0.25
-#define MAX_LED_PERCENT_DURATION 0.5
-
-// this is a number from 0..1 which represents how different 
-// the throttle sent to each motor can be during a half-rotation
-// 1 = very different
-// 0 = can't differ at all
-#define MELTY_MAX_TRANSLATION_AGGRESSION 0.75  
-#define MELTY_MAX_THROTTLE 1
-
-#define TANK_DRIVE_MAX_THROTTLE 0.125
-#define TANK_DRIVE_TURNING_MAX_THROTTLE 0.0625
-
-
-//----------END DONUT DRIVE SETTINGS---------
 
 
 //----------ACCELEROMETER SETTINGS----------
