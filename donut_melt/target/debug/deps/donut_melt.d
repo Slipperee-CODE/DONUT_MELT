a2d6f0e918070baccf48a6f98cc3a1da30ac3af0
cai <cai_scheidler@berkeley.edu>
2026-08-14 15:41:39 -0700

    ignored rust build files

--- donut_melt/target/debug/deps/donut_melt.d
+++
@@ -1,6 +0,0 @@
-C:\Users\CAIS\Documents\GitHubRepositories\DONUT_MELT\donut_melt\target\debug\deps\donut_melt.d: src\main.rs src\melty_drive.rs
-
-C:\Users\CAIS\Documents\GitHubRepositories\DONUT_MELT\donut_melt\target\debug\deps\donut_melt.exe: src\main.rs src\melty_drive.rs
-
-src\main.rs:
-src\melty_drive.rs:
